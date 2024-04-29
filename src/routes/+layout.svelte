2a0e5963002c551edb9a04ed3636c33d1f89f9cb
Jose A. Álvarez <infoiguacel@gmail.com>
2024-04-30 00:41:44 +0200

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class='app'>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	main {
+		width: 100%;
+		max-width: 72rem;
+		overflow: hidden;
+		padding: 2rem 0.5rem;
+		margin: 0 auto;
+		
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem;
+	}
+</style>
