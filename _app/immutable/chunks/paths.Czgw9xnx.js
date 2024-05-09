8742fecaabc183cab6a0b9e22bcc6fd16cadd17b
Jose A. Álvarez <infoiguacel@gmail.com>
2024-05-09 08:18:21 +0200

    Updates

--- _app/immutable/chunks/paths.Czgw9xnx.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_4x44nn)==null?void 0:s.base)??"/iguacel-pf";var a;const t=((a=globalThis.__sveltekit_4x44nn)==null?void 0:a.assets)??e;export{t as a,e as b};
