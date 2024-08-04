2cbbeeccba985545447637907221157170733940
Jose A. Álvarez <infoiguacel@gmail.com>
2024-08-04 11:09:45 +0200

    webp

--- src/lib/components/Promo.svelte
+++ src/lib/components/Promo.svelte
@@ -1,16 +1,18 @@
 <script>
-    import { base } from "$app/paths";
-    export let id;
-    const version = 1;
-    const format = "png";
-    export let name;
+  import { base } from "$app/paths";
+  export let id;
+  export let name;
+  const version = 1;
 </script>
 
-<img src="{base}/projects/{name}/{id}.{format}?v={version}" alt="">
+<picture>
+  <source srcset="{base}/projects/{name}/{id}.webp?v={version}" type="image/webp">
+  <img src="{base}/projects/{name}/{id}.png?v={version}" alt="" style="width: 100%; aspect-ratio: 16 / 9;">
+</picture>
 
 <style>
   img {
-  width: 100%;
-  aspect-ratio: 16 / 9;
+    width: 100%;
+    aspect-ratio: 16 / 9;
   }
 </style>
