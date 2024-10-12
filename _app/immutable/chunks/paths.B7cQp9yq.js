9904852db000d9ff74b35bdf669a29f204b9f8c0
Jose A. Álvarez <infoiguacel@gmail.com>
2024-10-12 09:24:57 +0200

    Updates

---
+++ _app/immutable/chunks/paths.B7cQp9yq.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_kbl5ha)==null?void 0:s.base)??"/iguacel-pf";var a;const l=((a=globalThis.__sveltekit_kbl5ha)==null?void 0:a.assets)??e;export{l as a,e as b};
---
+++ _app/immutable/entry/start.Cg_JyCCM.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C25AvJ0h.js";export{t as start};
